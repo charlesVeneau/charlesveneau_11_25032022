d942152a8315ad0efba1137af2bb63ef9c4e9ea0
CharlesVeneau <veneauc@hotmail.fr>
2022-03-28 22:37:36 +0200

    Create Header component, set Browser router

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './pages/Main';
+import { BrowserRouter } from 'react-router-dom'
+
 import './styles/mainStyle.scss'
 
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
--- src/pages/Main/index.jsx
+++ src/pages/Main/index.jsx
@@ -1,9 +1,10 @@
+import Header from '../../components/Header'
 import './index.scss'
 
 function App() {
   return (
     <div className="App">
-<h1 className="app-title">This is the App page</h1>
+<Header />
     </div>
   );
 }
