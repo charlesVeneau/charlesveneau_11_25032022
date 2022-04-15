09cade883b7753d9bb02c16a8c3dc5f81f4d2871
CharlesVeneau <veneauc@hotmail.fr>
2022-04-15 18:44:35 +0200

    Comments on Collapse animation function

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -12,6 +12,7 @@ function Collapse({ children, title, medium }) {
 
   function toggleCollapse(e) {
     e.target.classList.toggle('isClosed');
+    //Collapse animation handler
     expandeElement(e.target.nextElementSibling, 'isCollapsed');
   }
 
@@ -23,13 +24,16 @@ function Collapse({ children, title, medium }) {
     const startHeight = window.getComputedStyle(element).height;
     const startpadding = window.getComputedStyle(element).padding;
 
+    //Remove Collapsed class and save style
     element.classList.toggle(collapseClass);
     const height = window.getComputedStyle(element).height;
     const padding = window.getComputedStyle(element).padding;
 
+    //Set the start valur at the beginning of the transition
     element.style.height = startHeight;
     element.style.padding = startpadding;
 
+    // wait until the next frame so that everything has time to update before starting the transition
     requestAnimationFrame(() => {
       element.style.transition = '';
 
@@ -38,6 +42,8 @@ function Collapse({ children, title, medium }) {
         element.style.padding = padding;
       });
     });
+
+    //Listen to the transition to clear value once the transition is complete
     element.addEventListener('transitionned', () => {
       element.style.height = '';
       element.style.padding = '';
